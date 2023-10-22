0a807203d15e051fd20ef6b88aa354ecdfb5fb44
codemmk <2107228359@qq.com>
2023-10-22 20:49:26 +0800

    更改配置

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     Components({
